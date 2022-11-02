90f16290697a1a06f7d456faad6ae402405dfd95
GesS016 <gusikasadov@gmail.com>
2022-11-02 23:48:16 +0400

    Aviary small changes

--- Zoo/Aviary.cs
+++ Zoo/Aviary.cs
@@ -6,17 +6,17 @@ using System.Threading.Tasks;
 
 namespace Zoo
 {
-    public class Aviary
+    public class Aviary //Вольер
     {
-        public string AviaryName { get; protected set; }
-        public string AviaryBiome { get; protected set; }
-        public double AviarySquare { get; protected set; }
+        public string Name { get; protected set; }
+        public string Biome { get; protected set; }
+        public double Square { get; protected set; }
         
         public Aviary(string name, string biome, double square)
         {
-            AviaryName=name;
-            AviaryBiome=biome;
-            AviarySquare=square;
+            Name=name;
+            Biome=biome;
+            Square=square;
             List<AbstractAnimal> animals;
         }
     }
